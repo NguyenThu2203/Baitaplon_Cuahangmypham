f912cf66f790c4e57522cedbca910b8b16ab62df
VuThuong2003 <vuthithuong10523@gmail.com>
2024-05-31 18:34:10 +0700

    update frmkh

--- Baitaplon_Cuahangmypham/Class/Functions.cs
+++ Baitaplon_Cuahangmypham/Class/Functions.cs
@@ -15,9 +15,10 @@ namespace Baitaplon_Cuahangmypham.Class
             public static string connString;   //Khai báo biến chứa chuỗi kết nối
             public static void Connect()
             {
-             connString = "Data Source=DESKTOP-NM815JH\\SQLEXPRESS;Initial Catalog=CuaHangMyPham;Integrated Security=True;Encrypt=False"; 
+             connString = "Data Source=XIAODOU;Initial Catalog=CuaHangMyPham;Integrated Security=True;Encrypt=False"; 
             //connString = "Data Source=LAPTOP-9FSA2OM9\\SQLEXPRESS;Initial Catalog=CuaHangMyPham;Integrated Security=True;Encrypt=False";
-                Conn = new SqlConnection();                 //Cấp phát đối tượng
+             
+            Conn = new SqlConnection();                 //Cấp phát đối tượng
                 Conn.ConnectionString = connString;         //Kết nối
                 Conn.Open();                                //Mở kết nối
             }
